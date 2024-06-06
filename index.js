d71ae5b3de81b27e5f6ea917c422e680777e90fa
acadhrix2004 <163326329+Webtechcode1122@users.noreply.github.com>
2024-06-06 12:50:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // GSAP Scramble Text Effect
+    gsap.registerPlugin(TextPlugin, ScrollTrigger);
+
+    gsap.to(".scramble-text", {
+        duration: 2,
+        text: {
+            value: "Are you ready to explore the fascinating worlds of Metallurgy, Material Science, Geography, Biosciences, and more with AcaDhrix ?!!",
+            scrambleText: true
+        },
+        ease: "power2.inOut",
+        scrollTrigger: {
+            trigger: ".scramble-text",
+            start: "top 75%",
+        }
+    });
+
+    // GSAP Scroll Reveal Animations
+    gsap.utils.toArray(".fade-in-up").forEach((element) => {
+        gsap.fromTo(element, {
+            opacity: 0,
+            y: 20
+        }, {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            ease: "power2.out",
+            scrollTrigger: {
+                trigger: element,
+                start: "top 80%",
+                toggleActions: "play none none reverse"
+            }
+        });
+    });
+
+    // Logo Rotation Animation
+    gsap.to("#logo img", {
+        rotation: 360,
+        duration: 2,
+        repeat: -1,
+        ease: "linear"
+    });
+});
